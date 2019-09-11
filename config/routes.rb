6d124ef7e33949942b393bd8894e1295aee3d257
AMAN SHARMA <amansharmaammu2@gmail.com>
2019-09-11 11:15:04 +0530

    MAke it batter

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'home/ourservices'
   get 'home/under'
   get 'home/header'
+  get 'home/helper'
 
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
